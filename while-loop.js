62362f310053f074314c98b39b2ead3d9356d70d
Your Name <you@example.com>
2021-08-18 04:41:05 +0600

    while loop

---
+++ while-loop.js
@@ -0,0 +1,41 @@
+// var roastGiven = 0;
+// while (roastGiven < 10) {
+//     console.log("roast den please!!!!");
+//     console.log("polaow den please!!!!");
+//     console.log(roastGiven);
+//     //1 roastGiven = roastGiven + 1;
+//     //2 roastGiven += 1;
+//     roastGiven++;
+
+// }
+
+
+// var num1 = "99.5";
+// var num2 = 15;
+// console.log(num1 + num2);
+
+// var num = 0;
+//  while(num < 10) {num ++ }
+
+// for (i = 0; i < 10; i++) {
+
+//     console.log(i);
+// }
+
+// var name = "false";
+// console.log(typeof name);
+
+
+
+
+
+// var friend = ["a", "b", "c"];
+// var number = friend.indexOf("a");
+// console.log(number);
+
+
+
+for (i = 0; i < 10; i++) {
+
+    console.log(i);
+}
